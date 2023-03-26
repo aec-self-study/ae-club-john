42c324d35aec2c37ccb4ac2d47ccf3ad36bc5502
jan1365 <123534664+jan1365@users.noreply.github.com>
2023-03-26 03:33:44 +0000

    Add notes and remove NULL customer_id records

--- demo_dbt_project/models/sessions.sql
+++ demo_dbt_project/models/sessions.sql
@@ -4,8 +4,20 @@ select
     session_id,
     customer_id,
     visitor_id,
-    count(distinct id) as count_of_sessions,
+    device_type,
+    count(distinct page) as count_of_pages_visited,
     min(timestamp) as session_start_time,
-    max(timestamp) as session_end_time
+    max(timestamp) as session_end_time,
+    datetime_diff(max(timestamp), min(timestamp), second) as session_length_in_seconds,
+    array_agg(page order by timestamp)[safe_offset(0)] as first_page_visited,
+    array_reverse(array_agg(page order by timestamp))[safe_offset(0)] as last_page_visited,
+    --having a last page visited = 'order-confirmation' means that the session ended in a purchase
+    --this may not be the case, as the other pages may be visited after the 'order-confirmation' page in a given session 
+    case
+        when array_reverse(array_agg(page order by timestamp))[safe_offset(0)] = 'order-confirmation'
+        then true
+        else false
+    end as session_ended_in_purchase
 from {{ ref('web_tracking_pageviews') }}
-group by 1, 2, 3
+where customer_id is not null --visitor_id and session_id values are only applicable to those who have customer_id records
+group by 1, 2, 3, 4
